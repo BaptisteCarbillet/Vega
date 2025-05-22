a553321f8c393b36d56dd9bbc3fced202108a7a1
Baptiste <baptiste.carbillet@student-cs.fr>
2025-05-22 16:20:39 +0200

    added command to send angle to the gimbal

--- robomaster_sdk_can/inc/gimbal.hpp
+++ robomaster_sdk_can/inc/gimbal.hpp
@@ -33,7 +33,22 @@ public:
         pkg.write_to(_io);
     }
 
-    
+    void send_angles(int16_t yaw, int16_t pitch)
+    {
+        
+        uint8_t low_yaw = yaw & 0xFF;
+        uint8_t high_yaw = (yaw >> 8) & 0xFF;
+        uint8_t low_pitch = pitch & 0xFF;
+        uint8_t high_pitch = (pitch >> 8) & 0xFF;
+        robomaster::package pkg{0xC9, 0x04, 0x3f, 0xb0, false, false,
+            {
+            0x00, 0x08, 0x1d, low_yaw ,high_yaw, 0x00, 0x00, low_pitch, high_pitch, 
+            0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x1e, 0x00
+            }
+        };
+        
+        pkg.write_to(_io);
+    }
 
 private:
     std::iostream& _io;
