989ec285da786c988379f3dd7a346906f6c19994
Baptiste <baptiste.carbillet@student-cs.fr>
2025-05-22 15:51:07 +0200

    gimbal

---
+++ robomaster_sdk_can/inc/gimbal.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <protocol.hpp>
+#include <dds.hpp>
+
+namespace command
+{
+class gimbal
+{
+public:
+    gimbal(std::iostream& io)
+        : _io{io}
+    {}
+
+    
+
+    void send_workmode(uint8_t mode)
+    {
+        robomaster::package pkg{0x09, 0x04, 0x4, 0x4c, false, false};
+        pkg << mode << 0x00;
+        pkg.write_to(_io);
+    }
+
+    void recenter()
+    {
+        robomaster::package pkg{0xC9, 0x04, 0x3f, 0xb2, false, false,
+            {
+            0x00, 0x08, 0x05, 0x64, 0x00, 0x00, 0x00, 0x64, 0x00
+            }
+        };
+        
+        pkg.write_to(_io);
+    }
+
+    void send_speed(float vx, float vy, float omega)
+    {
+        robomaster::package pkg{0xC9, 0xC3, 0x3f, 0x21, false, false};
+        pkg << vx << vy << omega;
+        pkg.write_to(_io);
+    }
+
+private:
+    std::iostream& _io;
+};
+
+}
+
+
