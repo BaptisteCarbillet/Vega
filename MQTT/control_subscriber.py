db21326e74f82f59586c1d368349e4bd3d3bac66
BaptisteCarbillet <baptiste.carbillet@student-cs.fr>
2025-05-23 14:06:22 +0200

    decode command

--- MQTT/control_subscriber.py
+++ MQTT/control_subscriber.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt #import library
 import os
- 
+
 MQTT_SERVER = "argus.paris.inria.fr" #specify the broker address
 MQTT_PATH = "mqtt/control" #this is the name of topic
 
@@ -12,10 +12,11 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    
-    if msg.payload == 'STOP':
-        os.system('../robomaster_sdk_can/./stop_robot')
-    elif msg.payload == 'FORWARD':
+    command = msg.payload.decode('utf-8')
+    if command == 'STOP':
+        os.system('../robomaster_sdk_can/./stop_wheel')
+    elif command == 'FORWARD':
+        
         os.system('../robomaster_sdk_can/./mv_wheel 30 30 30 30')
 
  
