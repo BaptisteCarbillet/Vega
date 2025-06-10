faf26bbf6fd1ce399b92e00a17dfd6fdef379f08
Baptiste <baptiste.carbillet@student-cs.fr>
2025-06-10 17:00:33 +0200

    added option to save frame on press key

--- RTSPsub.py
+++ RTSPsub.py
@@ -5,7 +5,7 @@ pi_ip_addr = "128.93.82.122"
 rtsp_url = "rtsp://{}:8554/stream".format(pi_ip_addr)
 
 cap = cv2.VideoCapture(rtsp_url)
-
+i = 0
 # Set buffer size to reduce latency
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
@@ -17,10 +17,16 @@ while True:
         break
     
     cv2.imshow('RTSP Stream', frame)
-    
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    # Press 'q' to quit, 'w' to save the frame
+    if key == ord('q'):
         break
+    elif key == ord('w'):
+        # Save the current frame to a file
+        
+        cv2.imwrite('img/saved_frames/saved_frame{}.jpg'.format(i), frame)
+        i+=1
+        
 
 cap.release()
 cv2.destroyAllWindows()
