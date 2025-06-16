30d294c3af58594f6cc15acce0394188ce16d551
Baptiste <baptiste.carbillet@student-cs.fr>
2025-06-16 14:13:21 +0200

    added code to get ip addr of the RPBI'

---
+++ MQTT/ip_addr_publisher.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+import os
+broker = 'argus.paris.inria.fr'
+
+def on_publish(client, userdata, mid, reason_code, properties):
+    # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
+    try:
+        userdata.remove(mid)
+    except KeyError:
+        print('Error')
+        
+
+
+unacked_publish = set()
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_publish = on_publish
+
+mqttc.user_data_set(unacked_publish)
+
+mqttc.connect(broker)
+mqttc.loop_start()
+
+ip_addr = os.popen("hostname -I | awk '{print $1}'").read().strip()
+
+received = False
+
+ack_client = mqtt.Client()
+def on_message(client, userdata, msg):
+    global received
+    if msg.topic == "mqtt/ip_addr_ack":
+        print(f"Received acknowledgment for IP address: {msg.payload.decode()}")
+        received = True
+ack_client.on_message = on_message
+ack_client.connect(broker)
+ack_client.subscribe("mqtt/ip_addr_ack")
+ack_client.loop_start()
+
+
+
+while not received:
+    try:
+        mqttc.publish("mqtt/ip_addr",  ip_addr, qos=1)
+        
+    except Exception as e:
+        print(f"Error publishing IP address: {e}")
+        break
+
+ack_client.disconnect()
+mqttc.disconnect()
+
+
--- RTSPsub.py
+++ RTSPsub.py
@@ -1,7 +1,38 @@
 import cv2
+import paho.mqtt.client as mqtt
+import os
 
 
-pi_ip_addr = "128.93.82.122"
+
+
+broker = 'argus.paris.inria.fr'
+ip_receiver = mqtt.Client()
+ip_receiver.connect(broker)
+
+def on_message(client, userdata, msg):
+    global pi_ip_addr
+    if msg.topic == "mqtt/ip_addr":
+        pi_ip_addr = msg.payload.decode()
+        print(f"Received IP address: {pi_ip_addr}")
+        client.disconnect()
+
+ # Function to get the Raspberry Pi IP address
+
+ip_receiver.on_message = on_message
+ip_receiver.subscribe("mqtt/ip_addr")
+ip_receiver.loop_start()
+# Wait for the IP address to be received
+while 'pi_ip_addr' not in globals():
+    pass
+# Stop the loop once the IP address is received
+ip_receiver.loop_stop()
+
+#Send acknowledgment
+ack_client = mqtt.Client()
+ack_client.connect(broker)
+ack_client.publish("mqtt/ip_addr_ack", "IP address received", qos=1)
+ack_client.disconnect()
+
 rtsp_url = "rtsp://{}:8554/stream".format(pi_ip_addr)
 
 cap = cv2.VideoCapture(rtsp_url)
