c5a33bd9682a093b6e069273383af9af7562a83b
Baptiste <baptiste.carbillet@student-cs.fr>
2025-06-19 16:10:18 +0200

    latency experiment

---
+++ MQTT/latency_echo.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+import time
+from collections import deque
+
+MQTT_SERVER = "argus.paris.inria.fr"  # specify the broker address
+MQTT_PATH = "mqtt/latency"  # this is the name of topic
+client_send_ack = mqtt.Client()
+
+client_send_ack.connect(MQTT_SERVER + "_pong")
+
+def on_message(client, userdata, msg):
+    client_send_ack.publish(MQTT_PATH + "_pong", msg.payload)
+
+client_send_ack.on_message = on_message
+client_send_ack.subscribe(MQTT_PATH + "_ping")
+client_send_ack.loop_start()
