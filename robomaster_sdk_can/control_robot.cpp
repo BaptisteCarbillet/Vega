e39eee274ebec0788eaea9cc73dbfae64d377c03
BaptisteCarbillet <baptiste.carbillet@student-cs.fr>
2025-07-01 13:55:53 +0200

    controlling file with send_speed

--- robomaster_sdk_can/control_robot.cpp
+++ robomaster_sdk_can/control_robot.cpp
@@ -22,15 +22,29 @@ int main(int argc, char* argv[])
     for (int i = 1; i < argc; ++i)
     {
         std::string s = argv[i];
-        speed.push_back(std::stoi(s));
+        speed.push_back(std::stof(s));
     }
     float vx = speed[0];
     float vy = speed[1];
     float omega = speed[2];
-
+    float timeout = speed[3];
     chassis.send_workmode(1);
+    std::cout << "Setting speed: vx=" << vx << ", vy=" << vy << ", omega=" << omega << std::endl;
+    //while (true)
+    //{
+    //    chassis.send_heartbeat();
+     //   chassis.send_speed(vx, vy, omega);
+      //  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    //}
+    //write same loop but instead of while true do while time < timeout
 
-    chassis.send_speed(vx, vy, omega);
+    auto start = std::chrono::steady_clock::now();
+    while (std::chrono::steady_clock::now() - start < std::chrono::seconds(static_cast<int>(timeout)))
+    {
+        chassis.send_heartbeat();
+        chassis.send_speed(vx, vy, omega);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
     
     return 0;
 }
