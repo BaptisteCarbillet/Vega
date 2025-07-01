d10bef34102df19f527b6074b783a50af8fd1a1b
Baptiste <baptiste.carbillet@student-cs.fr>
2025-07-01 14:21:13 +0200

    visuo servo controller

--- vision/controller.py
+++ vision/controller.py
@@ -4,38 +4,38 @@ import subprocess
 import os 
 import time
 import subprocess
-
+from robomaster_sdk_can import Robot
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 BIN_PATH = os.path.join(SCRIPT_DIR, '../robomaster_sdk_can/')
 
-print(BIN_PATH)
+#print(BIN_PATH)
 class FrameProcessor:
 
     def __init__(self, frame):
         self.frame = frame
         self.reduced_frame = frame[300:, ::]  # Crop the image to reduce processing
-        self.edges = cv2.Canny(self.reduced_frame, 100, 200)
-        self.lines = cv2.HoughLines(self.edges, 1, np.pi / 180, threshold=100)
-
-    
-    
-    
-    def get_angle(self):
-        if self.lines is None:
-            self.angle = None
-        self.angle = self.deviation_from_vertical(self.lines)
-
-    @staticmethod
-    
-    def deviation_from_vertical(lines):
-        angle_deg = lines[:,0,1] * 180 / np.pi  # Convert radians to degrees
-        deviation = angle_deg   # since 90° (π/2) is vertical
-        # Wrap to [-90, +90]
-        deviation = np.where(deviation > 90, deviation - 180, deviation)
         
-        return deviation
-    
+    def process_frame(self):
 
+        self.reduced_frame = cv2.cvtColor(self.reduced_frame, cv2.COLOR_BGR2GRAY)
+        _, self.reduced_frame = cv2.threshold(self.reduced_frame, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        self.contours, _ = cv2.findContours(self.reduced_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    
+    def compute_centroid(self):
+        #Check that there is a contour
+        if self.contours:
+            for contour in self.contours:
+                M = cv2.moments(contour)
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    return cX, cY
+                else:
+                    print("Zero area contour, skipping.")
+        else:
+            
+            return None, None
 
 
 class PIDcontroller:
@@ -51,33 +51,31 @@ class PIDcontroller:
 
     def get_measurement(self):
         t0 = time.time()
-        subprocess.Popen([BIN_PATH + './center_gimbal'], shell=True)
-        subprocess.Popen([BIN_PATH + './mv_ginmbal -150 0'], shell=True)
-        #cap = cv2.VideoCapture(0)
-        #if not cap.isOpened():
-        #        raise Exception("Could not open video device")
-        ret, frame = self.cap.read()
         
+        ret, frame = self.cap.read()
         frame = cv2.resize(frame, (640, 480))
 
         self.frame = FrameProcessor(frame)
-        self.measurement = self.frame.get_angle() if self.frame.lines is not None else -1
-        t1 = time.time()
-        print(f"Frame processing time: {t1 - t0:.4f} seconds")
+        
+        self.frame.process_frame()
+        self.measurement = self.frame.compute_centroid()[0]
+        print(f"Frame processed in {time.time() - t0:.2f} seconds")
 
-    def compute(self, measurement):
-        error = self.setpoint - measurement
+    def compute(self):
+        error = self.setpoint - self.measurement
         self.integral += error
         derivative = error - self.previous_error
         
         output = (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative)
         
         self.previous_error = error
-        
+        print('output:', output)
         return output
     
-    
-controller = PIDcontroller(kp=1.0, ki=0.1, kd=0.05, setpoint=0.0)
+robot = Robot()   
+controller = PIDcontroller(kp=0.05, ki=0, kd=0.0, setpoint=320)
 while True:
     controller.get_measurement()
+    controller.compute()
+    robot.move_robot(0,output,0,1)
     
