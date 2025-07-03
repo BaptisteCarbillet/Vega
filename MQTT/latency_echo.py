66eeac554fdee21b1eac159b16b89f95f7f12964
Baptiste <baptiste.carbillet@student-cs.fr>
2025-07-03 16:45:28 +0200

    fixed latency code

--- MQTT/latency_echo.py
+++ MQTT/latency_echo.py
@@ -1,16 +1,16 @@
 import paho.mqtt.client as mqtt
 
 MQTT_SERVER = "argus.paris.inria.fr"  # specify the broker address
-MQTT_PATH = "mqtt/latency"  # this is the name of topic
+TOPIC = "mqtt/latency"  # this is the name of topic
 client_send_ack = mqtt.Client()
 
 # subscribe only when connected
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code", str(rc))
-    client.subscribe(MQTT_PATH)
+    client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
-    client_send_ack.publish(MQTT_PATH + "_ack", msg.payload)
+    client_send_ack.publish(TOPIC + "_ack", msg.payload)
 
 client_send_ack.on_connect = on_connect
 client_send_ack.on_message = on_message
--- MQTT/latency_measurement.py
+++ MQTT/latency_measurement.py
@@ -2,15 +2,15 @@ import paho.mqtt.client as mqtt
 import time
 import numpy as np
 from collections import deque
+import random
 
 MQTT_SERVER = "argus.paris.inria.fr"  # specify the broker address
-MQTT_PATH = "mqtt/latency"  # this is the name of topic
-
+TOPIC = "mqtt/latency"  # this is the name of topic
+TOPIC_ACK = TOPIC + "_ack"  # acknowledgment topic
 client = mqtt.Client()
 client.connect(MQTT_SERVER)
-client_ack = mqtt.Client()
-client_ack.connect(MQTT_SERVER)
-client_ack.subscribe(MQTT_PATH + "_ack")
+client.subscribe(TOPIC_ACK)
+
 current_time = time.time()
 latency_list = []
 msg_count = 0
@@ -19,28 +19,37 @@ def on_message(client, userdata, msg):
     global current_time
     global latency_list
     global msg_count
-    latency = time.time() - current_time
+    global time_sent
+    global random_payload
+    
+        
+    latency = time.time() - time_sent  # Calculate latency
+    assert msg.payload.decode('utf-8') == random_payload, "Payload mismatch"
     latency_list.append(latency)
     msg_count += 1
-    print(f"[{msg_count}] Latency: {latency:.6f}s")
+    
     if msg_count < 10000:
+        random_payload = str(random.randint(0, 10000)) #Generate a random payload
         current_time = time.time()  # Update the current time for the next message
-        client.publish(MQTT_PATH, '')
+        time_sent = time.time()  # Record the time when the message is sent
+        client.publish(TOPIC, random_payload) # Publish a message with the random payload
     else:
         print("Received 10000 messages, stopping...")
-        client_ack.loop_stop()
+        client.loop_stop()
 
-client_ack.on_message = on_message
-client_ack.subscribe(MQTT_PATH + "_ack")
-client_ack.loop_start()
+client.on_message = on_message
+client.loop_start()
 # wait for subscription to be ready before sending first message
 time.sleep(1)
-print("Sending initial message")
-current_time = time.time()
-client.publish(MQTT_PATH, '')  # Initial message to start the loop
+
+random_payload = str(random.randint(0, 10000))  # Generate a random payload
+time_sent = time.time()  # Record the time when the first message is sent
+client.publish(TOPIC, random_payload)  # Initial message to start the loop
 #to be able to echo 100000 messages
+
 while msg_count < 10000:
     time.sleep(0.1)  # Wait for some time to collect messages
+
 print('length of latency_list:', len(latency_list))
 if(latency_list):
     print("Average latency :  {} seconds".format(np.mean(latency_list)))
